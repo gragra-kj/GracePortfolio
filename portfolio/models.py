fdc971683349d3cbcb69fd9768d96eb58b71884d
gragra-kj <muthuigracia27@gmail.com>
2026-05-25 14:46:17 +0300

    profile model

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    fullname=models.CharField(max_length=50)
+    title=models.CharField(max_length=50)
+    bio=models.TextField()
+    profile_image=models.ImageField(upload_to='profile/')
+    email=models.EmailField()
+    git=models.URLField(blank=True)
+    linkedin=models.URLField(blank=True)
+    
+    def __str__(self):
+        return self.fullname
+    
+    
