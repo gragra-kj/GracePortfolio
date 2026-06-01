f56aeee4b3e58e0409bd5f3437698a3753886782
gragra-kj <muthuigracia27@gmail.com>
2026-06-01 12:02:37 +0300

    portfolio viewset

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .serializer import ProfileSerializer,ProjectSerializer,SkillsSerializer
+from .models import Project,Profile,Skills
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset=Profile.objects.all()
+    serializer_class=ProfileSerializer
+    
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset=Project.objects.all()
+    serializer_class=ProjectSerializer
+    
+class SkillViewSet(viewsets.ModelViewSet):
+    queryset=Skills.objects.all()
+    serializer_class=SkillsSerializer        
