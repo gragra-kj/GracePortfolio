5e43590e05af2f5c37f37990e021693722a0bc34
gragra-kj <muthuigracia27@gmail.com>
2026-06-01 11:54:09 +0300

    contact viewsets

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import ContactMessage
 
-# Create your views here.
+# Create your views here.from
+from .serializers import ContactSerializer
+
+class ContactViewSet(viewsets.ModelViewSet):
+    queryset=ContactMessage.objects.all()
+    serializer_class=ContactSerializer
