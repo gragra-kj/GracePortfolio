369d96910716ac6f5a270d4e35248e295d9e5d85
gragra-kj <muthuigracia27@gmail.com>
2026-07-07 12:41:19 +0300

    skills

--- frontend/src/components/Experience.jsx
+++ frontend/src/components/Experience.jsx
@@ -53,10 +53,52 @@ const Experience = () => {
         },
     ]
   return (
-    <section className=''>
-         
+    <section className="text-white py-20 relative overflow-hidden" id="skills">
+      <div className="max-w-7xl mx-auto px-6 lg:px-16 relative  z-10">
+        <div dat-aos="fade-right">
+          <h2 className="text-4xl md:text-5xl font-extrabold mb-12">
+            Technical <span className="text-primary"> Skills</span>
+          </h2>
+          <div className="space-y-8">
+            {skills.map((skill) => {
+              const SkillIcon = skill.icon;
+              return (
+                <div key={skill.id} className="group">
+                  <div className="flex items-center justify-between mb-2">
+                    <div className="flex items-center gap-3">
+                      <div
+                        className="p-2 bg-[#111a3e] rounded-lg
+                                        group-hover:bg-primary transition-colors duration-300"
+                      >
+                        <SkillIcon
+                          size={20}
+                          className="
+                                            text-primary group-hover:text-white"
+                        />
+                      </div>
+                      <span className="font-medium tracking-wide">
+                        {skill.name}
+                      </span>
+                    </div>
+                    <span className="text-primary font-bold">
+                      {skill.width}
+                    </span>
+                  </div>
+                  <div className="h-2 w-full bg-[#131d30] rounded-full">
+                    <div
+                      className="h-full rounded-full bg-linear-to-r
+                                        from-primary to-cyan-400 shadow-[0_0_10px_#06a2c2]"
+                      style={{ width: skill.width }}
+                    ></div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
 export default Experience
