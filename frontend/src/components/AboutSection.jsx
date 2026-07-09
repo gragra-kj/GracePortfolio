fbfa39eec619ac3e5e5fb28a4a2b63fce99608c7
gragra-kj <muthuigracia27@gmail.com>
2026-07-09 15:00:43 +0300

    about backend update

--- frontend/src/components/AboutSection.jsx
+++ frontend/src/components/AboutSection.jsx
@@ -1,6 +1,25 @@
 //import React from 'react'
 import { useEffect,useState } from "react";
+import api from "../api/portfolioApi";
 const AboutSection = () => {
+  const [profile,setProfile]=useState(null);
+
+  useEffect(()=>{
+    const fetchProfile=async()=>{
+      try{
+        const response=await api.get("profile/");
+        setProfile(response.data[0]);
+      }catch(error){
+        console.error(error);
+      }
+    };
+    fetchProfile()
+  },[]);
+  if(!profile){
+    return <div className="text-white">Loading....</div>
+  }
+
+
   return (
     <section className="text-white mt-20 relative overflow-hidden" id="about">
       <div
@@ -16,13 +35,8 @@ const AboutSection = () => {
           </h2>
           <p className='text-gray-400 text-base lg:text-lg
           mb-10 leading-relaxed'>
-            Detail-oriented IT Support Technician with experience
-            troubleshooting hardware, software, and network issues. Skilled in
-            technical support, system maintenance, and user support.
-            <br/><br/>
-            Knowledgeable in Python, Django, Django REST Framework, AWS, and
-            data analysis. Passionate about solving technical problems, building
-            practical technology solutions, and continuously expanding my skill
+            {profile.about}
+             
           </p>
           <div></div>
         </div>
