ed2c165c3d930f1d81722dc79ab54d80d191a2db
gragra-kj <muthuigracia27@gmail.com>
2026-07-17 13:39:10 +0300

    Prepare project for Render deployment

--- backend/settings.py
+++ backend/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 CORS_ALLOWED_ORIGINS=[
