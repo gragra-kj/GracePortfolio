87618289a5f0abbed5481e148005ba39a5715010
gragra-kj <muthuigracia27@gmail.com>
2026-07-17 14:13:16 +0300

    api changes

--- backend/urls.py
+++ backend/urls.py
@@ -1,28 +1,20 @@
-"""
-URL configuration for backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.http import JsonResponse
 from django.conf import settings
 from django.conf.urls.static import static
 
+def home(request):
+    return JsonResponse({
+        "message": "Grace Portfolio API is running!"
+    })
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/',include('portfolio.urls')),
-    path('api/contact/',include('contact.urls'))
+    path("", home),
+    path("admin/", admin.site.urls),
+    path("api/", include("portfolio.urls")),
+    path("api/contact/", include("contact.urls")),
 ]
-if settings.DEBUG:
-    urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+# Serve media files
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
