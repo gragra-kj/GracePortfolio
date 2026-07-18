391faf9727995c9ecb187b46ce5d3242196f8882
gragra-kj <muthuigracia27@gmail.com>
2026-07-18 21:46:27 +0300

    Serve media files on Render

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include,re_path
 from django.http import JsonResponse
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 def media_test(request):
     path = settings.MEDIA_ROOT / "profile" / "coding_programming_computer_technology_work.jpeg"
@@ -23,7 +24,13 @@ urlpatterns = [
     path("api/", include("portfolio.urls")),
     path("api/contact/", include("contact.urls")),
     path("media-test/", media_test),
+    
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.MEDIA_ROOT},
+    ),
 ]
 
 # Serve media files
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
