05f046d16b63044e2eb134fd95441e74a8d538c5
gragra-kj <muthuigracia27@gmail.com>
2026-07-18 21:39:05 +0300

    test media

--- backend/urls.py
+++ backend/urls.py
@@ -4,6 +4,14 @@ from django.http import JsonResponse
 from django.conf import settings
 from django.conf.urls.static import static
 
+def media_test(request):
+    path = settings.MEDIA_ROOT / "profile" / "coding_programming_computer_technology_work.jpeg"
+
+    return JsonResponse({
+        "exists": path.exists(),
+        "path": str(path),
+    })
+
 def home(request):
     return JsonResponse({
         "message": "Grace Portfolio API is running!"
@@ -14,6 +22,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("portfolio.urls")),
     path("api/contact/", include("contact.urls")),
+    path("media-test/", media_test),
 ]
 
 # Serve media files
